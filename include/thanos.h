e9848b3ac1a142376e276d155b4be58d82597579
Arnaud Vericel <arnaud.vericel@univ-lyon1.fr>
2020-12-17 20:36:54 +0100

    Added Thanos compilation option

---
+++ include/thanos.h
@@ -0,0 +1,34 @@
+#ifdef THANOS // only compile this part if thanos has been requested by user
+#ifndef THANOS_H
+#define THANOS_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Thanos {
+private:
+    // object
+    string filename;
+    vector<string> punchlines;
+
+    // private method used by constructor
+    void read_quotesfile();
+public:
+    // constructor
+    Thanos();
+
+    // destructor
+    ~Thanos() = default;
+
+    // methods
+    void print_artwork();
+    void talks_to_gamora();
+    void init_punchline();
+    void regular_ending();
+    void balanced_ending();
+};
+
+#endif
+#endif
